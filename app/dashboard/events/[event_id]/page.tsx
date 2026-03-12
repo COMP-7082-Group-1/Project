83283e469c2dde690abb0c409140b6cca4224c11
Henry Tan <henrytann00@gmail.com>
2026-03-11 23:25:28 -0700

    feat: skeleton for edit event

--- app/dashboard/events/[event_id]/page.tsx
+++ app/dashboard/events/[event_id]/page.tsx
@@ -1,5 +1,6 @@
 import { getEventByID } from "@/lib/data/eventByID";
 
+// todo: display event details
 export default async function EventPage({
   params,
 }: {
@@ -15,6 +16,7 @@ export default async function EventPage({
   return (
     <div className="flex flex-col gap-4">
       <h2 className="text-2xl font-semibold">{event.title}</h2>
+      <p className="text-muted-foreground">{event.description}</p>
     </div>
   );
 }
